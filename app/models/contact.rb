c9ea7c4d5b3dd017ec6d1d3dc895c5e71e720ff5
Marjorie Lam <marjielam@Marjories-MacBook-Pro.local>
2024-03-17 15:12:45 -0400

    Add method to create patient from contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,22 @@
 class Contact < ApplicationRecord
+  AVATAR_URL = "https://robohash.org/quitotamnon.png?size=300x300&set=set1".freeze
+
   belongs_to :patient, optional: true
 
   validates_presence_of :first_name, :last_name
   validates_uniqueness_of :patient, allow_nil: true
+
+  def create_patient!
+    return if patient
+
+    ActiveRecord::Base.transaction do
+      patient = Patient.create!(
+        first_name: first_name,
+        last_name: last_name,
+        avatar_url: AVATAR_URL,
+      )
+
+      update!(patient: patient)
+    end
+  end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -12,4 +12,30 @@ RSpec.describe Contact, type: :model do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
   end
+
+  describe "create_patient!" do
+    context "when the contact already has a patient attached" do
+      let(:contact) { FactoryBot.create(:contact, :with_patient) }
+
+      it "does nothing" do
+        initial_patient_id = contact.patient_id
+
+        expect { contact.create_patient! }.not_to change(Patient, :count)
+
+        expect(contact.reload.patient_id).to eq initial_patient_id
+      end
+    end
+
+    context "when the contact does not have a patient" do
+      let(:contact) { FactoryBot.create(:contact) }
+
+      it "creates and associates the patient" do
+        expect { contact.create_patient! }.to change(Patient, :count).by(1)
+
+        expect(contact.patient.first_name).to eq contact.first_name
+        expect(contact.patient.last_name).to eq contact.last_name
+        expect(contact.patient.avatar_url).to eq Contact::AVATAR_URL
+      end
+    end
+  end
 end
